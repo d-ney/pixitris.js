b7c4914bd980e787c153e2e1fda5411c982abf61
d-ney <d.ney210@gmail.com>
2024-01-25 08:20:19 -0800

    temp disable score for testing resizing

--- pixitris.js
+++ pixitris.js
@@ -186,14 +186,14 @@ let score_offset_x = 3*(screenWidth + scale)/4;
 let score_offset_y = screenHeight/8;
 let highscore = localStorage.getItem("highscore");
 if(highscore == null) localStorage.setItem("highscore", 0);
-console.log(highscore);
+//console.log(highscore);
 
 
 let score_text = new PIXI.Text(score,{fontFamily : 'Arial', fontSize: 24, fill : 0xff1010, align : 'right'});
-app.stage.addChild(score_text);
+//app.stage.addChild(score_text);
 
 let highscore_text = new PIXI.Text(highscore,{fontFamily : 'Arial', fontSize: 24, fill : 0xff1010, align : 'right'});
-app.stage.addChild(highscore_text);
+//app.stage.addChild(highscore_text);
 
 let current_piece_index = Math.floor(Math.random()  * 100) % 7;
 let current_rotation_index = 0;
@@ -504,7 +504,7 @@ function render() {
 
     //print_field();
     draw_pieces();
-    update_score();
+   // update_score();
    // draw_current_piece();
 }
 
